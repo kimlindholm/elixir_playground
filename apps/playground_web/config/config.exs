e6cd962d01eb585167f3cd3b404933b71c4bcd63
Kim Lindholm <misc@dynevision.com>
2018-11-21 11:10:19 +0700

    Run "mix phx.new.web playground_web"

---
+++ apps/playground_web/config/config.exs
@@ -0,0 +1,21 @@
+# Since configuration is shared in umbrella projects, this file
+# should only configure the :playground_web application itself
+# and only for organization purposes. All other config goes to
+# the umbrella root.
+use Mix.Config
+
+# General application configuration
+config :playground_web,
+  ecto_repos: [PlaygroundWeb.Repo],
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :playground_web, PlaygroundWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "ens2K8bIB3xfkyoCZ+rHScvfDSEDyvoMDZlmWikYkdK7N+620ukgJEVH2CSmXlSZ",
+  render_errors: [view: PlaygroundWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: PlaygroundWeb.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
