7d2f74636b0a2f9414c1dc0400ab03bd021e5493
Kim Lindholm <misc@dynevision.com>
2018-11-24 23:44:29 +0700

    Run "cd apps/playground_web && mix phx.gen.context Posts Post posts content:text"

---
+++ apps/playground_db/lib/playground_db/posts/post.ex
@@ -0,0 +1,18 @@
+defmodule PlaygroundDb.Posts.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "posts" do
+    field :content, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(post, attrs) do
+    post
+    |> cast(attrs, [:content])
+    |> validate_required([:content])
+  end
+end
