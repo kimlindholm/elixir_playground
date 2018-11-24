7041ee2b3b475ca9be70146dbc5fec0395238f80
Kim Lindholm <misc@dynevision.com>
2018-11-24 23:59:52 +0700

    Add Comment schema to Posts.Comments context

---
+++ apps/playground_db/lib/playground_db/posts/comments/comment.ex
@@ -0,0 +1,19 @@
+defmodule PlaygroundDb.Posts.Comments.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "comments" do
+    field :content, :string
+    field :post_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:content])
+    |> validate_required([:content])
+  end
+end
