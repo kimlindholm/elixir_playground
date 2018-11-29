7b2bfc410989f926de543d407019e37dc4307394
Kim Lindholm <misc@dynevision.com>
2018-11-29 13:48:16 +0700

    Add UI for listing posts

--- apps/playground_web/lib/playground_web/controllers/page_controller.ex
+++ apps/playground_web/lib/playground_web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule PlaygroundWeb.PageController do
   use PlaygroundWeb, :controller
+  alias Playground.Posts
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    with {:ok, posts} <- Posts.list_latest_posts() do
+      render(conn, "index.html", posts: posts)
+    else
+      {:error, code, msg} ->
+        render(conn, "index.html", error_code: code, error_msg: msg)
+    end
   end
 end
